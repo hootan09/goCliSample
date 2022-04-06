cb536cbf18c7660b2bfcddfa7e5fe4c8dedb766e
niki <hootan09@gmail.com>
2022-04-06 10:59:17 +0430

    add term flag for get joke

--- README.md
+++ README.md
@@ -23,4 +23,20 @@ Country Code: IR
 
 $ go run . getjoke
 A steak pun is a rare medium well done.
+
+$ go run . getjoke --term=hipster
+How did the hipster burn the roof of his mouth? He ate the pizza before it was cool.
+```
+
+We will be able to distribute this **go_cli** tool as a Go package. In order to do this:
+
+- Upload your code to a public repo
+- Install it by running **go get \<link-to-your-public-repo\>** e.g. go get github.com/example/go_cli
+
+Then you will be able to run your tool like this:
+```sh
+go_cli random
+
+# random joke with term
+go_cli getjoke --term=hipster
 ```
--- cmd/getjoke.go
+++ cmd/getjoke.go
@@ -9,7 +9,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -20,7 +22,14 @@ var getjokeCmd = &cobra.Command{
 	Short: "Get a random joke",
 	Long:  `This command fetches a random dad joke from the icanhazdadjoke api`,
 	Run: func(cmd *cobra.Command, args []string) {
-		getRandomJoke()
+
+		jokeTerm, _ := cmd.Flags().GetString("term")
+
+		if jokeTerm != "" {
+			getRandomJokeWithTerm(jokeTerm)
+		} else {
+			getRandomJoke()
+		}
 	},
 }
 
@@ -36,12 +45,25 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// getjokeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	getjokeCmd.Flags().String("term", "", "A search term for a dad joke.")
+}
+
+type SearchResult struct {
+	Results    json.RawMessage `json:"results"`
+	SearchTerm string          `json:"search_term"`
+	Status     int             `json:"status"`
+	TotalJokes int             `json:"total_jokes"`
 }
 
 type Joke struct {
 	ID     string `json:"id"`
 	Joke   string `json:"joke"`
 	Status int    `json:"status"`
+}
+
+func getRandomJokeWithTerm(jokeTerm string) {
+	total, results := getJokeDataWithTerm(jokeTerm)
+	randomiseJokeList(total, results)
 }
 
 func getRandomJoke() {
@@ -80,4 +102,37 @@ func getJokeData(baseApi string) []byte {
 		log.Printf("Could not read response body. %v", err)
 	}
 	return responseByte
+}
+
+func getJokeDataWithTerm(jokeTerm string) (totalJokes int, jokeList []Joke) {
+	url := fmt.Sprintf("https://icanhazdadjoke.com/search?term=%s", jokeTerm)
+	responseBytes := getJokeData(url)
+
+	jokeListRaw := SearchResult{}
+
+	if err := json.Unmarshal(responseBytes, &jokeListRaw); err != nil {
+		log.Printf("Could not unmarshal reponseBytes. %v", err)
+	}
+
+	jokes := []Joke{}
+	if err := json.Unmarshal(jokeListRaw.Results, &jokes); err != nil {
+		log.Printf("Could not unmarshal reponseBytes. %v", err)
+	}
+
+	return jokeListRaw.TotalJokes, jokes
+}
+
+func randomiseJokeList(length int, jokeList []Joke) {
+	rand.Seed(time.Now().Unix())
+
+	min := 0
+	max := length - 1
+
+	if length <= 0 {
+		err := fmt.Errorf("No jokes found with this term")
+		fmt.Println(err.Error())
+	} else {
+		randomNum := min + rand.Intn(max-min)
+		fmt.Println(jokeList[randomNum].Joke)
+	}
 }
